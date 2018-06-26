1cad19e026f99303904c7308b8ae53de2cc05828
John <compulsivecoder@gmail.com>
2018-06-26 09:56:35 +0000

    Trying to fix rollback script

--- rollback.sh
+++ rollback.sh
@@ -10,7 +10,7 @@ then
   git pull origin master && \
 
   echo "Resetting to previous revision" && \
-  git reset --hard HEAD~2 && \
+  git reset --hard HEAD~1 && \
   
   echo "Pushing back to origin/master" && \
   git push --force origin master && \
